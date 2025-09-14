c7bee5d387bd2e97701ab4b5b397975c6410a486
Abhishek sharma <abhishek.b.sharma1994@gmail.com>
2025-09-14 17:03:48 +0530

    added empty string test

--- spec/string_calculator_test.rb
+++ spec/string_calculator_test.rb
@@ -1,8 +1,13 @@
 require 'rspec'
 require "string_calculator"
 RSpec.describe StringCalculator do
+  subject(:calc) {describe_class.new}
   describe "#add" do
     it 'first test' do
+    end
+
+    it "return 0 if empty string" do
+      expect(calc.add("")).to eq 0
     end
   end
 end
